b8900eabe20adc2b1b14bcc2e1f0cf215a4ccf74
biqianhan-zidi <biqianhan-zidi@qq.com>
2020-01-22 19:52:59 +0800

    LeetCode

---
+++ LeetCode106 后序遍历与中序遍历生成二叉树.java
@@ -0,0 +1,48 @@
+
+/**
+ 根据一棵树的中序遍历与后序遍历构造二叉树。
+
+ 注意:
+ 你可以假设树中没有重复的元素。
+
+ 例如，给出
+
+ 中序遍历 inorder = [9,3,15,20,7]
+ 后序遍历 postorder = [9,15,7,20,3]
+ 返回如下的二叉树：
+
+ 3
+ / \
+ 9  20
+ /  \
+ 15   7
+
+*/
+import java.util.*;
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    Map<Integer,Integer> map;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i],i);
+        }
+        return Help(inorder,0,inorder.length,postorder,0,postorder.length);
+    }
+    private TreeNode Help(int[] inorder,int i_start,int i_end,int[] postoder,int p_start,int p_end){
+        if(i_start==i_end)return null;
+        int root_val = postoder[p_end-1];
+        int root_index = map.get(root_val);
+        TreeNode root = new TreeNode(root_val);
+        root.left = Help(inorder,i_start,root_index,postoder,p_start,p_start+root_index-i_start);
+        root.right = Help(inorder,root_index+1,i_end,postoder,p_start+root_index-i_start,p_end-1);
+        return root;
+    }
+}
+
---
+++ LeetCode107 二叉树的层次遍历 II.java
@@ -0,0 +1,54 @@
+/*
+给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+
+例如：
+给定二叉树 [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其自底向上的层次遍历为：
+
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+* */
+import java.util.*;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+
+}
+
+class Solution {
+    private List<List<Integer>> listlist;
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        listlist = new ArrayList<>();
+        if (root==null)return listlist;
+        Queue<TreeNode> queue = new ArrayDeque<>(50);
+        queue.add(root);
+        TreeNode in;
+        List<Integer> list = new ArrayList<>(50);
+
+        while (!queue.isEmpty()){
+            int leng = queue.size();
+            for (int i = 0; i < leng; i++) {
+                in = queue.remove();
+                if (in.left!=null)queue.add(in.left);
+                if (in.right!=null)queue.add(in.right);
+                list.add(in.val);
+            }
+            listlist.add(list);
+            list = new ArrayList<>();
+        }
+        Collections.reverse(listlist);
+        return listlist;
+    }
+}
